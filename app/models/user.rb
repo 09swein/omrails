8fd485cc8a0d03e61ee7360531eee3171831479f
Steven Weinberg <stevenj.weinberg@gmail.com>
2013-07-29 15:34:47 -0400

    Temporarily Removed Forgot Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :rememberable, :trackable, :validatable
+          #:recoverable,
   # attr_accessible :title, :body
 
   #fix for devise update from omrails notes
